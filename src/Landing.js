09ec663e0356ab299d29461c730e908e292c7570
Phuong Tran <pktran98@uw.edu>
2020-12-04 20:09:40 -0800

    landing component

---
+++ src/Landing.js
@@ -0,0 +1,31 @@
+import React, { lazy } from 'react';
+
+function LandingPage() {
+    return (
+        <div>
+            <section>
+                    <header className="splashHeader">
+                        Food Print
+                        <div>
+                                <ul>
+                                    <li>Home</li>
+                                    <li>About</li>
+                                </ul>
+                        </div>
+                    </header> 
+            </section>
+
+            <section id="main">
+                <div className="main-text">
+                    You want to <span>reduce</span> the carbon footprint of your food? <br />
+                    Focus on what you eat.
+                </div>
+                <div>
+                    <img className="image" src="./images/earth.jpg" width="520" alt="An image of green movement" />
+                </div>
+            </section>
+        </div>
+    );   
+}
+
+export default LandingPage;
