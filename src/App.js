b250688355ef690a8ff425d88ba3ba3ea02fdbb2
strozj <strozj@uw.edu>
2020-12-06 15:08:19 -0800

    made card generation dynamic with a JSON files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import React from 'react' ;
+import {React, useState} from 'react' ;
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom'; 
 import {LandingPage, NavBar} from './Landing.js';
-import { RenderCards } from './CardSection.js';
+import { CardList } from './CardSection.js';
 //import { AddCardForm } from './CardForms.js';
 import { CheckoutPage } from './CartCheckout.js';
 
-
-
-function App() {
-
-    const eventHandle = (event) =>  {
-         
-    }
-    
-
+function App(props) {
     return (
         <div>
             <NavBar />
             <Switch>
                 <Route exact path="/"><LandingPage/></Route>
-<<<<<<< HEAD
-                <Route path="/explore"><RenderCards props={eventHandle}/></Route>
-=======
-                <Route path="/explore"><RenderCards/></Route>
+                <Route path="/explore"><CardList foods={props.foods}/></Route>
                 <Route path="/cart"><CheckoutPage/></Route>
->>>>>>> 9645c04e6122b66d5830e49721cc4ff991c2f661
             </Switch>
-            
         </div>
     )
 }
-// <RenderCards cards={props.cards} />
 
 export default App;
--- src/CardSection.js
+++ src/CardSection.js
@@ -1,60 +1,21 @@
 import React from 'react';
 
-function RenderCards(props) {
-
-    const [list, updateList] = useState([]);
-
-    const handleClick = (event) => {
-        updateList(list.concat(event))
-        console.log(list);
-    }
-
+function Card(props) {
     return (
-        <div className="card-container">
-                <div className="card meat" id = "Beef_beef_herd">
-                    <img className="card-image meat" src="./images/Beef_beef_herd.jpg" alt="raw beef steaks" id = "beef"/>
-                </div>
-                <div className="card others" id = "Cheese">
-                    <img className="card-image others" src="./images/Cheese.jpg" alt="cheese wheels"/>
-                </div>
-                <div className="card others" id = "Dark_Chocolate">
-                    <img className="card-image others" src="./images/Dark_Chocolate.jpg" alt="choclate bars"/>
-                </div>
-                <div className="card others" id = "Coffee">
-                    <img className="card-image others" src="./images/Coffee.jpg" alt="coffee beans"/>
-                </div>
-                <div className="card meat" id = "Pig_Meat">
-                    <img className="card-image meat" src="./images/Pig_Meat.jpg" alt="bacon on a grill"/>
-                </div>
-                <div className="card seafood" id = "Shrimps_farmed">
-                    <img className="card-image seafood" src="./images/Shrimps_farmed.jpg" alt="raw prawns"/>
-                </div>
-                <div className="card meat" id = "Poultry_Meat">
-                    <img className="card-image meat" src="./images/Poultry_Meat.jpg" alt="raw chicken breasts"/>
-                </div>
-                <div className="card meat" id = "Lamb__Mutton">
-                    <img className="card-image meat" src="./images/Lamb__Mutton.jpg" alt="raw lamb chops"/>
-                </div>
-
-                <div className="card others" id = "Palm_Oil">
-                    <img className="card-image others" src="./images/Palm_Oil.jpg" alt="palm oil and seeds"/>
-                </div>
-                <div className="card others" id = "Eggs">
-                    <img className="card-image others" src="./images/Eggs.jpg" alt="eggs in a carton"/>
-                </div>
-            <form>
-                <button className="btn btn-primary">Save</button>
-            </form>
+        <div className="card" id={props.food.name}>
+            <img className="card-image" src={props.food.img} alt={props.food.name}/>
         </div>
     )
 }
 
-// function AddCardForm() {
-//     return (
-//         <form>
-//             <button className="btn btn-primary">Save</button>
-//         </form>
-//     );
-// }
+function CardList(props) {
+    return (
+        <div className="card-container">
+            {props.foods.map((food) => {
+                return <Card food={food}/>
+            })}
+        </div>
+    )
+}
 
-export {RenderCards};
+export {CardList};
