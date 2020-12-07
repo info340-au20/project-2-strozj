46bba77306993df9cc517a732b6a4fe73aef45ab
strozj <strozj@uw.edu>
2020-12-06 17:05:43 -0800

    added state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react' ;
+import React, { useState} from 'react' ;
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom'; 
 import {LandingPage, NavBar} from './Landing.js';
@@ -7,12 +7,21 @@ import { CardList } from './CardSection.js';
 import { CheckoutPage } from './CartCheckout.js';
 
 function App(props) {
+
+    const [state, setState] = useState([]);
+
+
+    const handleClick = (event) => {
+        console.log(event.target.id);
+        setState(state.concat(event.target.id));
+    }
+
     return (
         <div>
             <NavBar />
             <Switch>
                 <Route exact path="/"><LandingPage/></Route>
-                <Route path="/explore"><CardList foods={props.foods}/></Route>
+                <Route path="/explore"><CardList foods={props.foods} callback={handleClick}/></Route>
                 <Route path="/cart"><CheckoutPage/></Route>
             </Switch>
         </div>
