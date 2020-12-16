fffc8ef19f5e949198c13b6faeb5863d927b0653
strozj <strozj@uw.edu>
2020-12-15 17:12:52 -0800

    added daily, weekly, and monthly calculations for carbon emissions

--- src/App.js
+++ src/App.js
@@ -9,14 +9,56 @@ import { ComputeDay, ComputeWeek, ComputeMonth } from './CarbonComputer.js';
 function App(props) {
 
     const [state, setState] = useState([]);
-
     const [clickCount, setClickCount] = useState(0);
+
+    const [handleDay, setHandleDay] = useState(false);
+
+    const [handleWeek, setHandleWeek] = useState(false);
+
+    const [handleMonth, setHandleMonth] = useState(false);
 
     const handleClick = (event) => {
         if (!state.includes(event.target.id)) {
             setClickCount(clickCount + 1)
             setState(state.concat(event.target.id));
         }
+    }
+
+    const handleDayToggle = () => {
+        if (handleDay) {
+            setHandleDay(false);
+        } else {
+            setHandleDay(true);
+        }
+    }
+
+    const handleWeekToggle = () => {
+        if (handleWeek) {
+            setHandleWeek(false);
+        } else {
+            setHandleWeek(true);
+        }
+    }
+
+    const handleMonthToggle = () => {
+        if (handleMonth) {
+            setHandleMonth(false);
+        } else {
+            setHandleMonth(true);
+        }
+    }
+
+    let dayComponent;
+    let weekComponent;
+    let monthComponent;
+    if (handleDay) {
+        dayComponent = <ComputeDay items={state} foods={props.foods} />
+    }
+    if (handleWeek) {
+        weekComponent = <ComputeWeek items={state} foods={props.foods} />
+    }
+    if (handleMonth) {
+        monthComponent = <ComputeMonth items={state} foods={props.foods} />
     }
     
     return (
@@ -25,10 +67,11 @@ function App(props) {
             <Switch>
                 <Route exact path="/"><LandingPage/></Route>
                 <Route path="/explore"><Prompt clicks={clickCount}/><CardList foods={props.foods} callback={handleClick}/></Route>
-                <Route path="/profile"><Profile items={state} foods={props.foods} 
-                callbackDay={<ComputeDay items={state} foods={props.foods}/>}
-                callbackMonth={<ComputeMonth items={state} foods={props.foods}/>}
-                callbackWeek={<ComputeWeek items={state} foods={props.foods}/>}/></Route>
+                <Route path="/profile"><Profile items={state} foods={props.foods} callbackDay={handleDayToggle} callbackMonth={handleMonthToggle} callbackWeek={handleWeekToggle}/>
+                {dayComponent}
+                {weekComponent}
+                {monthComponent}
+                </Route>
             </Switch>
         </div>
     )
--- src/MyPage.js
+++ src/MyPage.js
@@ -19,9 +19,9 @@ export function Profile(props) {
                 </ul>
             </div>
             <div id="profileButton">
-                <Button className="addButton" style={{ color: "#769E90"}} size="lg" onClick={callbackDay}> Daily </Button>  &nbsp;&nbsp;&nbsp;
-                <Button className="addButton" style={{ color: "#769E90"}} size="lg" onClick={callbackWeekly}> Weekly </Button>  &nbsp;&nbsp;&nbsp;
-                <Button className="addButton" style={{ color: "#769E90"}} size="lg" onClick={callbackMonthly}> Monthly </Button> &nbsp;&nbsp;&nbsp;
+                <Button className="addButton" style={{ color: "#769E90"}} size="lg" onClick={props.callbackDay}> Daily </Button>  &nbsp;&nbsp;&nbsp;
+                <Button className="addButton" style={{ color: "#769E90"}} size="lg" onClick={props.callbackWeek}> Weekly </Button>  &nbsp;&nbsp;&nbsp;
+                <Button className="addButton" style={{ color: "#769E90"}} size="lg" onClick={props.callbackMonth}> Monthly </Button> &nbsp;&nbsp;&nbsp;
             </div>
         </div>
     );  
