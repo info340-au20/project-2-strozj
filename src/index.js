dc99d7996e7d2f787b79dd8a5721912e1bb9e50d
Phuong Tran <pktran98@uw.edu>
2020-12-16 16:51:49 -0800

    add fire base sdk

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'; 
+import firebase from 'firebase';
 
 import FOOD from './food.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcj8y3kMWPoRI1uQZSmewWU33s6c5xaY8",
+  authDomain: "food-print-724e6.firebaseapp.com",
+  projectId: "food-print-724e6",
+  storageBucket: "food-print-724e6.appspot.com",
+  messagingSenderId: "1035916557929",
+  appId: "1:1035916557929:web:78dc06624e98b49fec6cf8",
+  measurementId: "G-724122JF9E"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <BrowserRouter>
